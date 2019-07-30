d30a81f03f7ac96d8ba842fd113af69feceff823
Nick Letwin <nletwin@uber.com>
2019-07-30 12:27:46 -0400

    Scaffoled apps

---
+++ web/jstars/node_modules/fusion-cli/test/e2e/gql/fixture/src/main.js
@@ -0,0 +1,27 @@
+// @noflow
+import App from 'fusion-core';
+// $FlowFixMe
+import {gql} from 'fusion-apollo';
+const schema = gql('./schema.graphql');
+const query = gql('./query.gql');
+
+if (__BROWSER__) {
+  window.schema = schema;
+  window.query = query;
+}
+
+export default (async function() {
+  const app = new App('element', el => el);
+  __NODE__ &&
+    app.middleware((ctx, next) => {
+      if (ctx.url === '/schema') {
+        ctx.body = schema;
+      }
+      if (ctx.url === '/query') {
+        ctx.body = query;
+      }
+      return next();
+    });
+
+  return app;
+});
